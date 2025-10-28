1587e6813114108b85f95aa0e758345e924b2a50
lucasshaz9-star <lucasshaz9@gmail.com>
2025-10-28 17:15:29 +0600

    25-62065-2

---
+++ main1.java
@@ -0,0 +1,11 @@
+
+public class main1 {
+ public static void main1(String[] args) {
+  int price = 250;
+  
+  float discount = 0.15F;javac
+  float Final = discount * price;
+ 
+  System.out.println(Final);
+ }   
+}
